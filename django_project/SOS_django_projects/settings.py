0ff48ccc47a7ddef3fc9006085719f0cf27e242b
pooja01-sahu <poojasahu11296@gmail.com>
2026-08-06 14:15:16 +0530

    Update settings.py

--- django_project/SOS_django_projects/settings.py
+++ django_project/SOS_django_projects/settings.py
@@ -79,8 +79,9 @@ SIMPLE_JWT = {
     "USER_ID_CLAIM": "user_id",
 }
 
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:4200",
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",  
+    "http://localhost",        
 ]
 
 SESSION_SERIALIZER = "django.contrib.sessions.serializers.JSONSerializer"
